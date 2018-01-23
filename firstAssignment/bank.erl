99a27ad39ca29edbfa58b6e7c636c3cbd3a9619f
Lasse Sjöblom <larseriksjoblom@gmail.com>
2018-01-23 15:44:59 +0100

    stuffs!

--- firstAssignment/bank.erl
+++ firstAssignment/bank.erl
@@ -2,49 +2,80 @@
 -compile(export_all).
 
 test() ->
-    io:format("Starting tests~n"),
     Pid = new(),
-    io:format("Testing add~n"),
+    % Testing add
     ok = add(Pid, 10),
     ok = add(Pid, 20),
-    io:format("Testing balance~n"),
+    cant_add_below_zero = add(Pid, -100),
     30 = balance(Pid),
-    io:format("Testing withdraw~n"),
+    %testing withdraw
     ok = withdraw(Pid, 15),
-    io:format("Testing balance after withdraw~n"),
+    cant_withdraw_below_zero = withdraw(Pid, -100),
     15 = balance(Pid),
     insufficient_funds = withdraw(Pid, 20),
-    horray.
+    ok = withdraw(Pid, 15),
+
+
+    % incorrect param tests
+    {'EXIT', _} = (catch add(Pid, asd)),
+    {'EXIT', _} = (catch balance(asd)),
+    {'EXIT', _} = (catch withdraw(Pid, asd)),
+
+    % all tests done
+    hooray.
 
 new() ->
+    % Creates a new 'bank' with 0 as current funds.
     spawn(fun() -> bank(0) end).
 
 balance(Pid) -> 
     rpc(Pid, balance).
 
 add(Pid, Amount) -> 
-    rpc(Pid, {add, Amount}).
+    case is_integer(Amount) of
+        true -> rpc(Pid, {add, Amount});
+        false -> erlang:error(badarg)
+    end.
 
 withdraw(Pid, Amount) -> 
-    rpc(Pid, {withdraw, Amount}).
+    case is_integer(Amount) of
+        true -> rpc(Pid, {withdraw, Amount});
+        false -> erlang:error(badarg)
+    end.
 
 rpc(Pid, Msg) ->
-    %% sends Msg to Pid and recieves the response.
+    % sends Msg to Pid and recieves the response.
     Pid ! {self(), Msg},
     receive Any -> Any end.
 
 bank(X) ->
-    io:format("inside bank~n", []),
     receive
+        % add messages
+        {From, {add, Y}} when Y < 0 ->
+            From ! cant_add_below_zero,
+            bank(X);
         {From, {add, Y}} -> 
             From ! ok, 
             bank(X+Y);
+        
+        % Withdraw messages
         {From, {withdraw, Y}} when Y > X ->
-            From ! insufficient_funds;
+            From ! insufficient_funds,
+            bank(X);
+        {From, {withdraw, Y}} when Y < 0 ->
+            From ! cant_withdraw_below_zero,
+            bank(X);
         {From, {withdraw, Y}} -> 
             From ! ok, 
             bank(X-Y);
+
+        % Balance messages
         {From, balance} -> 
             From ! X,
+            bank(X);
+        
+        % Catch anything undefined
+        {From, _ } ->
+            From ! erlang:error(badarg),
             bank(X)
     end.
--- firstAssignment/multi_bank.erl
+++ firstAssignment/multi_bank.erl
@@ -3,35 +3,70 @@
 
 test() ->
     Pid = new(),
-    %% account creation tests
+
+        %% account creation tests
     ok = createAccount(Pid, lasse),
     duplicate_account_error = createAccount(Pid, lasse),
     ok = createAccount(Pid, maria),
 
-    % test add balance & withdraw
+        % test add balance & withdraw
     ok = add(Pid, lasse, 100),
     100 = balance(Pid, lasse),
     ok = add (Pid, maria, 200),
     200 = balance(Pid, maria),
     no_known_account_error = balance(Pid, joe),
-    0 = withdraw(Pid, lasse, 100),
-    100 = withdraw(Pid, maria, 100),
+    ok = withdraw(Pid, lasse, 100),
+    ok = withdraw(Pid, maria, 100),
+
+        % Lend tests
+    ok = lend(Pid, maria, lasse, 50),
+    50 = balance(Pid, maria),
+    50 = balance(Pid, lasse),
+
+        % delete account test
+    remaining_funds_error = deleteAcount(Pid, lasse),
+    ok = withdraw(Pid, lasse, 50),
+    ok = deleteAcount(Pid, lasse),
+    [maria] = listAccounts(Pid),
+
+        % incorrect param tests
+    {'EXIT', _} = (catch add(Pid, lasse, asd)),
+    {'EXIT', _} = (catch balance(asd, asd)),
+    {'EXIT', _} = (catch withdraw(Pid, maria, asd)),
+    {'EXIT', _} = (catch add(Pid, joe, asd)),
+    {'EXIT', _} = (catch withdraw(Pid, joe, asd)),
+
     horray.
 
 new() ->
     spawn(fun() -> bank(maps:new()) end).
 
+listAccounts(Pid) ->
+    rpc(Pid, listAccounts).
+
+deleteAcount(Pid, Who) -> 
+    rpc(Pid, {deleteAcount, Who}).
+
 lend(Pid, From, To, Amount) ->
-    rpc(Pid, {From, To, Amount} ).
+    case is_integer(Amount) of
+        true -> rpc(Pid, {lend, From, To, Amount});
+        false -> erlang:error(badarg)
+    end.
 
 balance(Pid, Who) -> 
     rpc(Pid, {balance, Who} ).
 
 add(Pid, Who, Amount) -> 
-    rpc(Pid, {add, Who, Amount}).
+    case is_integer(Amount) of
+        true -> rpc(Pid, {add, Who, Amount});
+        false -> erlang:error(badarg)
+    end.
 
 withdraw(Pid, Who, Amount) -> 
-    rpc(Pid, {withdraw, Who, Amount}).
+    case is_integer(Amount) of 
+        true -> rpc(Pid, {withdraw, Who, Amount});
+        false -> erlang:error(badarg)
+    end.
 
 
 createAccount(Pid, Who) ->
@@ -42,72 +77,107 @@ rpc(Pid, Msg) ->
     Pid ! {self(), Msg},
     receive Any -> Any end.
 
-bank(X) ->
+
+bank(AccountsMap) ->
     receive
-        %% account creation
+
+            % Account related operations
+
         {From, {createAccount, Name}} ->
-        case maps:is_key(Name, X) of
+        case maps:is_key(Name, AccountsMap) of
             true -> 
                 From ! duplicate_account_error, 
-                bank(X);
+                bank(AccountsMap);
             false -> 
                 From ! ok,
-                bank(maps:put(Name, 0, X))
+                bank(maps:put(Name, 0, AccountsMap))
         end;
 
-        %% Amount operations
-
-        {From, {add, Name, Amount}} -> 
-            case maps:is_key(Name, X) of 
+        {From, {deleteAcount, Name}} ->
+        % 
+        case maps:is_key(Name, AccountsMap) of
+            true -> 
+                case maps:get(Name, AccountsMap) > 0 of 
                 true -> 
+                    From ! remaining_funds_error, 
+                    bank(AccountsMap);
+                false -> 
+                    NewMap = maps:remove(Name, AccountsMap),
+                    From ! ok, 
+                    bank(NewMap)
+                end;
+            false ->
+                From ! no_known_account_error,
+                bank(AccountsMap)
+        end;
+
+        {From, listAccounts} -> 
+            From ! maps:keys(AccountsMap),
+            bank(AccountsMap);
+
+
+            % Amount operations
+            
+        {From, {add, Name, Amount}} -> 
+            %adds Amount to Name if it exists.
+            case maps:is_key(Name, AccountsMap) of 
+                true ->  % Account exists, increase amount bound to Name.
                     From ! ok,
-                    NewBalance = maps:get(Name, X) + Amount,
-                    NewMap = maps:update(Name, NewBalance, X),
+                    NewBalance = maps:get(Name, AccountsMap) + Amount,
+                    NewMap = maps:update(Name, NewBalance, AccountsMap),
                     bank(NewMap);
-                false ->
+                false -> 
                     From ! no_known_account_error,
-                    bank(X)
+                    bank(AccountsMap)
             end;
 
         {From, {withdraw, Name, Amount}} ->
-            case maps:is_key(Name, X) of
+        % Reduces the amount specified from the Name account if enough funds is present.
+            case maps:is_key(Name, AccountsMap) of
                 true -> 
                     %% account exists, check if balance is enough
-                    case maps:get(Name, X) > Amount of 
-                        true -> 
-                            NewBalance = maps:get(Name, X) - Amount,
-                            NewMap = maps:update(Name, NewBalance, X),
+                    case maps:get(Name, AccountsMap) >= Amount of 
+                        true -> % Everything checks out, reduce Amount from Name.
+                            NewBalance = maps:get(Name, AccountsMap) - Amount,
+                            NewMap = maps:update(Name, NewBalance, AccountsMap),
                             From ! ok,
                             bank(NewMap);
-                        false ->
+                        false -> % Not enough funds
                             From ! insufficient_funds,
-                            bank(X)
+                            bank(AccountsMap)
                     end;
-                false -> 
+                false -> % Account does not exist
                     From ! no_known_account_error,
-                    bank(X)
+                    bank(AccountsMap)
             end;
     
         {From, {lend, Lender, Borrower, Amount}} ->
-            case maps:is_key(Lender, X) of
-                true -> case maps:is_key(Borrower, X) of
-                        true -> case maps:get(Lender, X) > Amount of 
-                                true ->
-                                    LenderBalance = maps:get(Lender, X) - Amount,
-                                    BorrowerBalance = maps:get(Borrower, X) + Amount,
-                                    
-                                false -> From ! insufficient_funds, bank(X),
-                        false-> From ! no_known_account_error, bank(X),
-                false -> From ! no_known_account_error, bank(X)
+        % Tests if accounts Lender and Borrower exists, then checks if Lender has enough funds 
+        % to Borrow Amount. Lastly Transfers Amount from Lender To borrower
+            case maps:is_key(Lender, AccountsMap) of
+                true -> case maps:is_key(Borrower, AccountsMap) of
+                        true -> case maps:get(Lender, AccountsMap) > Amount of 
+                                true -> % This is where the actual lend operation takes place
+                                    LenderBalance = maps:get(Lender, AccountsMap) - Amount,
+                                    BorrowerBalance = maps:get(Borrower, AccountsMap) + Amount,
+                                    NewMap = maps:update(Lender, LenderBalance, AccountsMap),
+                                    From ! ok,
+                                    bank(maps:update(Borrower, BorrowerBalance, NewMap));
+                                false -> From ! insufficient_funds, bank(AccountsMap) % Lender insufficient_funds
+                                end;
+                        false -> From ! no_known_account_error, bank(AccountsMap) % Borrower does not exist
+                        end;
+                false -> From ! no_known_account_error, bank(AccountsMap) % Lender does not exist
             end; % end lend
             
         {From, {balance, Name}} -> 
-            case maps:is_key(Name, X) of
+        % Check if Name is a valid account if true return balance, otherwise return no_known_account_error.
+            case maps:is_key(Name, AccountsMap) of
                 true -> 
-                    From ! maps:get(Name, X),
-                    bank(X);
+                    From ! maps:get(Name, AccountsMap),
+                    bank(AccountsMap);
                 false -> 
                     From ! no_known_account_error,
-                    bank(X)
+                    bank(AccountsMap)
             end % end balance
         end. % recieve end
